dec8026cc5e41460718f39c304beff190088ccc8
Jake Lauer <jake.lauer27@gmail.com>
2018-09-08 23:42:00 -0600

    Add Up Arrow toggle for charts

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
-document.querySelector('.settings-container').addEventListener('click', hideNav)
+document.querySelector('.settings-container').addEventListener('click', toggleNav)
+document.querySelector('.section-break-carrot').addEventListener('click', toggleCharts)
 
-function hideNav(){
+function toggleNav(){
   document.querySelector('.sidebar').classList.toggle('hide');
   document.querySelector('body').classList.toggle('expand-body')
+}
+
+function toggleCharts() {
+  document.querySelector('.charts-section').classList.toggle('toggle-up');
+  document.querySelector('.section-break-carrot').classList.toggle('toggle-up-arrow');
+  document.querySelector('.charts-mini-menu').classList.toggle('toggle-up')
+
 }
