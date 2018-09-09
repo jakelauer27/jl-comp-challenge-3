31fc820f80dfb2c842361d2d56b15227d444cdab
Jake Lauer <jake.lauer27@gmail.com>
2018-09-09 13:38:54 -0600

    Add data to table

--- scripts.js
+++ scripts.js
@@ -11,10 +11,10 @@ function toggleCharts() {
 }
 
 function showPopup(bar) {
-  bar.parentNode.insertAdjacentHTML("afterend", `<div class="bar-info-popup" style="bottom: calc(33px + (1.63 * 63px))">
-  <h4>Wednesday, Dec 3rd</h4>
-  <p class="bar-info-text"><span class="blue-text">382</span>
-  Applicants / <span class="blue-text">37</span>
+  bar.parentNode.insertAdjacentHTML("afterend", `<div class="bar-info-popup" style="bottom: calc(40px + ${bar.getAttribute('height')}px)">
+  <h4>${bar.getAttribute('data-day')}</h4>
+  <p class="bar-info-text"><span class="blue-text">${bar.getAttribute('data-applicants')}</span>
+  Applicants / <span class="blue-text">${bar.getAttribute('data-interviews')}</span>
   Interviews
   </p>
   <div class="triangle"></div>
@@ -24,3 +24,4 @@ function showPopup(bar) {
 function removePopup(bar) {
   bar.parentNode.nextSibling.remove();
 }
+
